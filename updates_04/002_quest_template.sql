2f62d13bdde8e47c208965cdc2192486647d96fc
Valcorb <valcorb@hotmail.com>
2013-02-16 05:06:13 +0100

    Fix class & race restriction for quest "Join the battle!"

---
+++ updates_04/002_quest_template.sql
@@ -0,0 +1,6 @@
+UPDATE `quest_template` SET `RequiredClasses`=1, `RequiredRaces`=1101 WHERE `Id`=28789; -- Warrior
+UPDATE `quest_template` SET `RequiredClasses`=2, `RequiredRaces`=1101 WHERE `Id`=28785; -- Paladin
+UPDATE `quest_template` SET `RequiredClasses`=4, `RequiredRaces`=1101 WHERE `Id`=28780; -- Hunter
+UPDATE `quest_template` SET `RequiredClasses`=16, `RequiredRaces`=1101 WHERE `Id`=28786; -- Priest
+UPDATE `quest_template` SET `RequiredClasses`=128, `RequiredRaces`=1101 WHERE `Id`=28784; -- Mage
+UPDATE `quest_template` SET `RequiredClasses`=256, `RequiredRaces`=1101 WHERE `Id`=28788; -- Warlock
