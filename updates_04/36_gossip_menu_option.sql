bd46e98e982ed72e34a0fcd24c84618579db2714
DarkAngel <darkangel39@abv.bg>
2013-03-10 16:55:35 +0200

    Corrected a typo in the comments.

--- updates_04/36_gossip_menu_option.sql
+++ updates_04/36_gossip_menu_option.sql
@@ -56,6 +56,6 @@ UPDATE `gossip_menu_option` SET `option_id`=3,`npc_option_npcflag`=128 WHERE `me
 UPDATE `gossip_menu_option` SET `option_id`=8,`npc_option_npcflag`=65536 WHERE `menu_id`=11504 AND `id`=0;
 UPDATE `gossip_menu_option` SET `option_id`=3,`npc_option_npcflag`=128 WHERE `menu_id`=11504 AND `id`=1;
 
- -- Several Innkeepers sue this menu
+ -- Several Innkeepers use this menu
 UPDATE `gossip_menu_option` SET `option_id`=8,`npc_option_npcflag`=65536 WHERE `menu_id`=344 AND `id`=0;
 UPDATE `gossip_menu_option` SET `option_id`=3,`npc_option_npcflag`=128 WHERE `menu_id`=344 AND `id`=1;
