d41577e499e76537f72a87f3189307e40185d754
Nay <dnpd.dd@gmail.com>
2013-07-17 01:00:27 +0100

    TDB 434.05

--- updates/000_version.sql
+++ updates/000_version.sql
@@ -1,6 +1,6 @@
 UPDATE `version` SET
-    `db_version` = 'TDB 434.04',
-    `cache_id` = 4
+    `db_version` = 'TDB 434.05',
+    `cache_id` = 5
 WHERE
-    `db_version` = 'TDB 434.03'
+    `db_version` = 'TDB 434.04'
 LIMIT 1;
