b8075bf1d94e2ae2f1e282cc2f759accd247c539
Exodius <nickey_c_q@yahoo.co.uk>
2013-08-04 19:34:17 +0100

    Correct drop rates for items related to quest "Zenn's Bidding"
    Closes #252

--- updates/035_loot_drop.sql
+++ updates/035_loot_drop.sql
@@ -1,3 +1,6 @@
--- Webwood Spider Silk has Very High drop rate
--- http://eu.battle.net/wow/en/item/3412
-UPDATE `creature_loot_template` SET `ChanceOrQuestChance`=-90 WHERE `entry` IN (1998,1999,2000,2001) and `item`=3412;
+-- Fel Moss (3297) has Guaranteed drop rate
+-- Nightsaber Fang (3409) and Strigid Owl Feather (3409) have Very High drop rate
+-- http://eu.battle.net/wow/en/item/3297
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance`=-100 WHERE `entry` IN (1988,1989) and `item`=3297;
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance`=-90 WHERE `entry`IN (2033,2034,2042,2043,14430) and `item`=3409;
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance`=-90 WHERE `entry` IN (1995,1996,1997) and `item`=3411;
