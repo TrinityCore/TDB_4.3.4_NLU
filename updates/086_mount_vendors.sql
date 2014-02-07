2d857bc56dad3fc3739ca759c9798c82808a4d5b
Ukulutl <gascart@gmail.com>
2014-02-07 08:20:16 +0100

    correct mount vendors flags

---
+++ updates/086_mount_vendors.sql
@@ -0,0 +1,14 @@
+UPDATE `creature_template` SET `npcflag`=`npcflag` | 1 | 128, `ScriptName` = 'npc_mount_vendor' WHERE  `entry` = 48510; -- Kall Worthaton
+UPDATE `creature_template` SET `npcflag`=`npcflag` | 1 | 128, `ScriptName` = 'npc_mount_vendor' WHERE  `entry` = 17584; -- Torallius the Pack Handler
+UPDATE `creature_template` SET `npcflag`=`npcflag` | 1 | 128, `ScriptName` = 'npc_mount_vendor' WHERE  `entry` = 16264; -- Winaestra
+UPDATE `creature_template` SET `npcflag`=`npcflag` | 1 | 128, `ScriptName` = 'npc_mount_vendor' WHERE  `entry` = 7955; -- Milli Featherwhistle
+UPDATE `creature_template` SET `npcflag`=`npcflag` | 1 | 128, `ScriptName` = 'npc_mount_vendor' WHERE  `entry` = 7952; -- Zjolnir
+UPDATE `creature_template` SET `npcflag`=`npcflag` | 1 | 128, `ScriptName` = 'npc_mount_vendor' WHERE  `entry` = 4731; -- Zachariah Post
+UPDATE `creature_template` SET `npcflag`=`npcflag` | 1 | 128, `ScriptName` = 'npc_mount_vendor' WHERE  `entry` = 4730; -- Lelanai
+UPDATE `creature_template` SET `npcflag`=`npcflag` | 1 | 128, `ScriptName` = 'npc_mount_vendor' WHERE  `entry` = 3685; -- Harb Clawhoof
+UPDATE `creature_template` SET `npcflag`=`npcflag` | 1 | 128, `ScriptName` = 'npc_mount_vendor' WHERE  `entry` = 3362; -- Ogunaro Wolfrunner
+UPDATE `creature_template` SET `npcflag`=`npcflag` | 1 | 128, `ScriptName` = 'npc_mount_vendor' WHERE  `entry` = 1261; -- Veron Amberstill
+UPDATE `creature_template` SET `npcflag`=`npcflag` | 1 | 128, `ScriptName` = 'npc_mount_vendor' WHERE  `entry` = 4885; -- Gregor MacVince
+UPDATE `creature_template` SET `npcflag`=`npcflag` | 1 | 128, `ScriptName` = 'npc_mount_vendor' WHERE  `entry` = 2357; -- Merideth Carlson
+UPDATE `creature_template` SET `npcflag`=`npcflag` | 1 | 128, `ScriptName` = 'npc_mount_vendor' WHERE  `entry` = 1460; -- Unger Statforth
+UPDATE `creature_template` SET `npcflag`=`npcflag` | 1 | 128, `ScriptName` = 'npc_mount_vendor' WHERE  `entry` = 384; -- Katie Hunter
