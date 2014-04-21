32c69a509af209b01a229b30c6ae3c38880db442
Vincent_Michael <Vincent_Michael@gmx.de>
2014-04-21 04:33:31 +0200

    Fix sai error

--- updates/091_2014_01_26_07_world_spell_script_names.sql
+++ updates/091_2014_01_26_07_world_spell_script_names.sql
@@ -1,6 +1,6 @@
-DELETE FROM `spell_script_names` WHERE `spell_id` IN (-34861,64844,64904,54968,71610,71641);
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (34861,64844,64904,54968,71610,71641);
 INSERT INTO `spell_script_names` (`spell_id` ,`ScriptName`) VALUES
-(-34861,'spell_pri_circle_of_healing'),
+(34861,'spell_pri_circle_of_healing'),
 (64844, 'spell_pri_divine_hymn'),
 (64904, 'spell_pri_hymn_of_hope'),
 (54968, 'spell_pal_glyph_of_holy_light'),
