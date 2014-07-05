9ed862fd81b731559b6f56245d30292aa25572b8
Vincent_Michael <Vincent_Michael@gmx.de>
2014-07-05 23:13:17 +0200

    Fix fail in 911049c4d1614a6e6fbb1957cfe951186b5cd475

--- updates/138_2014_06_23_07_world_misc_335a.sql
+++ updates/138_2014_06_23_07_world_misc_335a.sql
@@ -1,4 +1,5 @@
 -- Texts needs to be updated for cata (no BroadcastTextID because this reason)
+/*
 UPDATE `gameobject_template` SET `AIName`= 'SmartGameObjectAI' WHERE  `entry`=148498;
 
 DELETE FROM `smart_scripts` WHERE `entryorguid` =148498;
@@ -53,3 +54,4 @@ UPDATE `creature_template` SET `AIName`= 'SmartAI' WHERE  `entry`=8417;
 DELETE FROM `smart_scripts` WHERE `entryorguid` =8417;
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (8417,0,0,0,19,0,100,0,3566,0,0,0,12,8391,3,300000,0,0,0,8,0,0,0,-6460.528,-1267.63,180.7818,1.89,' Dying Archaeologist - On Quest Accept - spawn Lathoric the Black');
+*/
