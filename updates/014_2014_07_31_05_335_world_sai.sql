1c77f4c8a8ac5824fe0dfb47c1c03431c9bd45e0
Vincent_Michael <Vincent_Michael@gmx.de>
2014-08-18 19:55:43 +0200

    Fix typo

--- updates/014_2014_07_31_05_335_world_sai.sql
+++ updates/014_2014_07_31_05_335_world_sai.sql
@@ -1,4 +1,4 @@
-UPDATE `creature_text` SET `type`=14 WHERE  `entry`=8391 AND `groupid`=1 AND `id`=0;
+/*UPDATE `creature_text` SET `type`=14 WHERE  `entry`=8391 AND `groupid`=1 AND `id`=0;
 
 DELETE FROM `creature_text` WHERE `entry`IN(8421, 8391);
 INSERT INTO `creature_text` (`entry`, `groupid`, `id`, `text`, `type`, `language`, `probability`, `emote`, `duration`, `sound`, `comment`, `BroadcastTextID`) VALUES 
@@ -71,3 +71,4 @@ DELETE FROM `smart_scripts` WHERE `entryorguid` =8421;
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (8421, 0,0,1,11,0,100,1,0,0,0,0,8,1,0,0,0,0,0,1,0,0,0,0,0,0,0,'Dorius - On Spawn - Set React State Defensive'),
 (8421, 0,1,0,61,0,100,1,0,0,0,0,2,120,0,0,0,0,0,1,0,0,0,0,0,0,0,'Dorius - On Spawn - Set Faction');
+*/
