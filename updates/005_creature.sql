a397a7a963bfcb2e0d1ec702c62c00d1ca3e538b
Aokromes <Aokromes@users.noreply.github.com>
2014-08-27 13:02:11 +0200

    Update 005_creature.sql

--- updates/005_creature.sql
+++ updates/005_creature.sql
@@ -1,5 +1,5 @@
 --
-SET @GUID = 338125;
+SET @CGUID = 338125;
 DELETE FROM `creature` WHERE `guid` BETWEEN @CGUID+0 AND @CGUID+2;
 INSERT INTO `creature` (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `MovementType`) VALUES
 (@CGUID+0, 51787, 1, 1, 1, -523.3464, -4278.454, 41.72705, 0.113011, 120, 5, 1), -- 51787 (Area: 6451) (possible waypoints or random movement)
