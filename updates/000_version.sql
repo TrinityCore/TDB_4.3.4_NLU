3d7251328a5277d258935960b149fa127f1a3421
Vincent_Michael <Vincent_Michael@gmx.de>
2014-09-21 13:47:45 +0200

    TDB 434.08 - 2014/09/21

--- updates/000_version.sql
+++ updates/000_version.sql
@@ -1,6 +1,6 @@
 UPDATE `version` SET
-    `db_version` = 'TDB 434.07',
-    `cache_id` = 7
+    `db_version` = 'TDB 434.08',
+    `cache_id` = 8
 WHERE
-    `db_version` = 'TDB 434.06'
+    `db_version` = 'TDB 434.07'
 LIMIT 1;
